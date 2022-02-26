ddaf1219e60f1667f0951d3fac412812a1abcbe4
srnelapatla <srnelapatla@gmail.com>
2022-02-25 20:59:42 -0600

    String Palindrome example

--- StrPalindrome.py
+++ StrPalindrome.py
@@ -3,6 +3,13 @@
 from Tools.scripts.fixcid import Reverse
 
 str1 = input("enter the palindrome : ")
+str2 = str1[len(str1)::-1]  ## or str1[::-1]
+if str1 == str2:
+    print(str1, "is a Palindrome ")
+else:
+    print(str1, "is not a Palindrome ")
+"""
+following is the another method to find if the string is a palindrome or not
 thisList = list(str1)
 # print(thisList)
 newlist = thisList[::-1]
@@ -12,3 +19,4 @@ if "".join(thisList) == "".join(newlist):
     print(str1, "is a Palindrome ")
 else:
     print(str1, "is not a Palindrome ")
+"""
