dd1f7bfd93c1eeae91354bb429d03543cc31ac17
srnelapatla <100450644+srnelapatla@users.noreply.github.com>
2022-02-26 20:31:24 -0600

    Update AnimalTest.py

--- pythonClasses/AnimalTest.py
+++ pythonClasses/AnimalTest.py
@@ -2,7 +2,9 @@ from pythonClasses.Animal import Animal
 
 
 class AnimalTest:
+#creating dog object for the Animal class
     dog = Animal("scooby")
+ #type returns the class name of the object dog
     print(type(dog))
     dog.set_animal_color("Brown")
     print(dog.name, "color is ", dog.get_animal_color())
