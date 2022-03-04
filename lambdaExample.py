864ae7896f4cb4f9b55ef0d8e07baeedec555ce4
srnelapatla <srnelapatla@gmail.com>
2022-03-04 15:10:15 -0600

    lambda example

--- lambdaExample.py
+++ lambdaExample.py
@@ -1,25 +1,23 @@
 """
-lamda is the function without a name
+lambda is the function without a name
 """
+
 
 ## add two number with function
 
 
-
-def addTwoNumber(a,b):
+def addTwoNumber(a, b):
     return a + b
 
-print(addTwoNumber(1,5)) #this returns 6
 
-#now with lamda
-sum = lambda  x, y : x + y
+print(addTwoNumber(1, 5))  # this returns 6
 
-print(sum(1,5))
+# now with lambda
+sum = lambda x, y: x + y
 
+print(sum(1, 5))
 
-#now with lamda
-operations = lambda  x, y : (x + y , x -y , x * y)
+# now with lambda
+operations = lambda x, y: (x + y, x - y, x * y)
 
-
-print(operations(1,5))
-
+print(operations(1, 5))
