cf49186710401a68a7a7927d4fcdf70e3e6ebfee
Santosh Nelapatla <srnelapatla@santoshs-air.lan>
2022-03-03 21:14:53 -0600

    about private variable

---
+++ pythonClasses/PrivateVariableEx.py
@@ -0,0 +1,13 @@
+#this is the example about private variable.
+#private variable is defined using "__variableName"
+class PrivateVariableEx:
+    def __init__(self):
+        self.var = "something"
+        #declare private variable using double underscrore
+        self.__privateVar = "PrivateVar"
+
+pve = PrivateVariableEx()
+
+print(pve.var)
+##print(pve.privateVar)
+"""uncommenting private variable access retunrs an error"""
