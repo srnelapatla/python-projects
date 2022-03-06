528174031fe4910f4045cdbfb5fd53762427bf83
srnelapatla <srnelapatla@gmail.com>
2022-03-06 14:02:58 -0600

    Multiple inheritance example

---
+++ pythonClasses/MultipleInheritanceParentClassB.py
@@ -0,0 +1,6 @@
+#Multiple inheritance example
+# child class inherit the properties from more than one parent class
+
+class MultipleInheritanceParentClassB:
+    def __init__(self):
+        self.classNameB = "MultipleInheritanceParentClassB"
