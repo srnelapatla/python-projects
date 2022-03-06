8cc223b55b2d37f4b4dd20277f14db2b7259e569
srnelapatla <srnelapatla@gmail.com>
2022-03-06 13:57:42 -0600

    Fibonacci number

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,10 +1,16 @@
-#the rule that each number (called a Fibonacci number) is equal to the sum of the preceding two numbers. ...
-#F (0) = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+# the rule that each number (called a Fibonacci number) is equal to the sum of the preceding two numbers. ...
+# F (0) = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
 
+x, y = 0, 1
+while y < 50:
+    print(y, end=',')
+    x, y = y, x + y
+
+# another way...
 
 startNumber = 0
 nextNumber = 1
-#printing 20 fibonacci number
+# printing 20 fibonacci number
 print(startNumber, end=',')
 
 for i in range(20):
@@ -12,6 +18,3 @@ for i in range(20):
     print(sumOfPrevTwoNumbers, end=',')
     nextNumber = startNumber
     startNumber = sumOfPrevTwoNumbers
-
-
-
