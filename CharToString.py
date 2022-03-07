cfb0088f71ac6e4aada952eefb6fb92af3695471
srnelapatla <srnelapatla@gmail.com>
2022-03-07 08:52:06 -0600

    char to string using join udemy section 5-probme 37

---
+++ CharToString.py
@@ -0,0 +1,10 @@
+# Convert a list of characters into a string
+# Input ['a', 'b', 'c', 'd']
+# Output abcd
+
+str1 = ['a', 'b', 'c', 'd']
+print(''.join(str1))
+
+# this is to seperate the each char by colon or any other char
+#str='santosh'
+#print(':'.join(str)) # returns s:a:n:t:o:s:h
---
+++ CntOfWordsInAStr.py
@@ -0,0 +1,17 @@
+#count the number of words in a string
+
+str = "this is the longest string I typed but I might have used longest time"
+c = 0
+for words in str.split(" "):
+    c += 1
+print (c)
+
+
+# get the unique word count in a string
+
+t =()
+c = 0
+for word in str.split(" "):
+    t..add(word)
+    c += 1
+print (c)
